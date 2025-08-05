79f9e8e431a246719d97e5b57207f928dd82fe78
tbrewer <tbrewer@torchtechnologies.com>
2025-08-05 14:38:57 -0500

    Created and updated visual assets and text

--- core/src/main/java/com/tbrewer/idlemess/GameScreen.java
+++ core/src/main/java/com/tbrewer/idlemess/GameScreen.java
@@ -8,52 +8,49 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class GameScreen implements Screen {
     final Drop game;
 
     private final Texture backgroundTexture;
-    private final Texture bucketTexture;
-    private final Texture dropTexture;
+    private final Texture ship1Texture;
+    private final Texture laser1Texture;
     private final Sound dropSound;
     private final Music music;
-    private final Sprite bucketSprite;
+    private final Sprite ship1Sprite;
     private final Vector2 touchPos;
-    private final Array<Sprite> dropSprites;
+    private final Array<Sprite> laser1Sprites;
     private float dropTimer;
-    private final Rectangle bucketRectangle;
-    private final Rectangle dropRectangle;
+    private final Rectangle ship1Rectangle;
+    private final Rectangle laserRectangle;
     private int dropsGathered;
 
     public GameScreen(final Drop game){
         this.game = game;
 
-        backgroundTexture = new Texture("images/background.png");
-        bucketTexture = new Texture("images/bucket.png");
-        dropTexture = new Texture("images/drop.png");
+        backgroundTexture = new Texture("images/Background1.png");
+        ship1Texture = new Texture("images/Ship1.png");
+        laser1Texture = new Texture("images/laser1.png");
 
         dropSound = Gdx.audio.newSound(Gdx.files.internal("sounds/drop.mp3"));
         music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
         music.setLooping(true);
         music.setVolume(.5f);
 
-        bucketSprite = new Sprite(bucketTexture);
-        bucketSprite.setSize(1,1);
-        bucketSprite.rotate90(true);
+        ship1Sprite = new Sprite(ship1Texture);
+        ship1Sprite.setSize(1,.5f);
 
         touchPos = new Vector2();
 
-        bucketRectangle = new Rectangle();
-        dropRectangle = new Rectangle();
+        ship1Rectangle = new Rectangle();
+        laserRectangle = new Rectangle();
 
-        dropSprites = new Array<>();
+        laser1Sprites = new Array<>();
 
     }
 
@@ -77,16 +74,16 @@ public class GameScreen implements Screen {
         float delta = Gdx.graphics.getDeltaTime(); // retrieve the current delta
 
         if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            bucketSprite.translateY(speed * delta); // Move the bucket right
+            ship1Sprite.translateY(speed * delta); // Move the bucket right
         }else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            bucketSprite.translateY(-speed * delta);
+            ship1Sprite.translateY(-speed * delta);
         }
 
         if(Gdx.input.isTouched()){
             // If the user has clicked of tapped the screen
             touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // Get where the touch happened on screen
             game.viewport.unproject(touchPos); // Convert the units to the world units of the viewport
-            bucketSprite.setCenterY(touchPos.y); // Change the Vertical centered position of the bucket
+            ship1Sprite.setCenterY(touchPos.y); // Change the Vertical centered position of the bucket
         }
     }
 
@@ -96,34 +93,34 @@ public class GameScreen implements Screen {
         float worldHeight = game.viewport.getWorldHeight();
 
         // Store the bucket size for brevity
-        float bucketWidth = bucketSprite.getWidth();
-        float bucketHeight = bucketSprite.getHeight();
+        float bucketWidth = ship1Sprite.getWidth();
+        float bucketHeight = ship1Sprite.getHeight();
 
         // Clamp x to values between 0 and worldWidth
-        bucketSprite.setY(MathUtils.clamp(bucketSprite.getY(), 0, worldHeight - bucketWidth));
+        ship1Sprite.setY(MathUtils.clamp(ship1Sprite.getY(), 0, worldHeight - bucketWidth));
 
         float delta = Gdx.graphics.getDeltaTime(); // retrieve the current delta
 
         // Apply the bucket position and size to the bucketRectangle
-        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
+        ship1Rectangle.set(ship1Sprite.getX(), ship1Sprite.getY(), bucketWidth, bucketHeight);
 
         // Loop through the sprites backwards to prevent out of bounds errors
-        for(int i = dropSprites.size -1; i >= 0; i--) {
-            Sprite dropSprite = dropSprites.get(i); // Get the sprite from the list
-            float dropWidth = dropSprite.getWidth();
-            float dropHeight = dropSprite.getHeight();
+        for(int i = laser1Sprites.size -1; i >= 0; i--) {
+            Sprite laserSprite = laser1Sprites.get(i); // Get the sprite from the list
+            float laserWidth = laserSprite.getWidth();
+            float laserHeight = laserSprite.getHeight();
 
-            dropSprite.translateX(-2f * delta);
+            laserSprite.translateX(-2f * delta);
 
             //Apply the drop position and size to the dropRectangle
-            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
+            laserRectangle.set(laserSprite.getX(), laserSprite.getY(), laserWidth, laserHeight);
 
             // if the top of the drop goes below the bottom of the view, remove it
-            if (dropSprite.getY() < -dropHeight) {
-                dropSprites.removeIndex(i);
-            }else if(bucketRectangle.overlaps(dropRectangle)) {
+            if (laserSprite.getY() < -laserHeight) {
+                laser1Sprites.removeIndex(i);
+            }else if(ship1Rectangle.overlaps(laserRectangle)) {
                 dropsGathered++;
-                dropSprites.removeIndex(i);
+                laser1Sprites.removeIndex(i);
                 dropSound.play();
             }
         }
@@ -131,24 +128,23 @@ public class GameScreen implements Screen {
         dropTimer += delta;     // Adds the current delta to the timer
         if(dropTimer > 1f) {    // Check if it has been more than a second
             dropTimer = 0;      // Reset the timer
-            createDroplet();    // Create the droplet
+            createLaser();    // Create the droplet
         }
     }
 
-    private void createDroplet(){
+    private void createLaser(){
         // create local variables for convenience
         float dropWidth = 1;
-        float dropHeight = 1;
+        float dropHeight = .5f;
         float worldWidth = game.viewport.getWorldWidth();
         float worldHeight = game.viewport.getWorldHeight();
 
         // create the drop sprite
-        Sprite dropSprite = new Sprite(dropTexture);
-        dropSprite.setSize(dropWidth, dropHeight);
-        dropSprite.setX(worldWidth); // randomize the drop's x position
-        dropSprite.setY(MathUtils.random(0f, worldHeight - dropHeight));
-        dropSprite.rotate90(true);
-        dropSprites.add(dropSprite);
+        Sprite laser1Sprite = new Sprite(laser1Texture);
+        laser1Sprite.setSize(dropWidth, dropHeight);
+        laser1Sprite.setX(worldWidth); // randomize the drop's x position
+        laser1Sprite.setY(MathUtils.random(0f, worldHeight - dropHeight));
+        laser1Sprites.add(laser1Sprite);
     }
 
     private void draw(){
@@ -163,12 +159,12 @@ public class GameScreen implements Screen {
 
         game.batch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); // draw the background
         //spriteBatch.draw(bucketTexture, 0, 0, 1, 1); // draw the bucket with width/height of 1 meter
-        bucketSprite.draw(game.batch); // Sprites have their own draw method
+        ship1Sprite.draw(game.batch); // Sprites have their own draw method
 
-        game.font.draw(game.batch, "Drops collected: " + dropsGathered, 0, worldHeight);
+        game.font.draw(game.batch, "Number of lasers hit: " + dropsGathered, 0, worldHeight);
 
         // draw each sprite
-        for( Sprite dropSprite : dropSprites) {
+        for( Sprite dropSprite : laser1Sprites) {
             dropSprite.draw(game.batch);
         }
 
@@ -201,7 +197,7 @@ public class GameScreen implements Screen {
         backgroundTexture.dispose();
         dropSound.dispose();
         music.dispose();
-        dropTexture.dispose();
-        bucketTexture.dispose();
+        laser1Texture.dispose();
+        ship1Texture.dispose();
     }
 }
